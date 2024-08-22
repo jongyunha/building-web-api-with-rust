56f19560c264380bc8ab100be62cf455d3db71bc
jongyunha <jongyun@seoulrobotics.org>
2024-08-22 22:56:50 +0900

    feat: setup rocket

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
+use rocket::routes;
+use rocket_db_pools::{Connection, Database};
+
 mod model;
 mod schema;
 mod repositories;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Database)]
+#[database("postgres")]
+struct DbConn(rocket_db_pools::diesel::PgPool);
+#[rocket::get("/rustaceans")]
+fn get_rustaceans(db: &Connection<DbConn>) {
+
+}
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build()
+        .mount("/", routes![
+            get_rustaceans
+        ])
+        .attach(DbConn::init())
+        .launch()
+        .await;
 }
